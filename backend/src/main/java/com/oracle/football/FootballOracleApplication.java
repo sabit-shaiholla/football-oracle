caafe23e79145439f8d25e1c8369fdc4aee3f683
sabit-shaiholla <sabit.shaiholla@nu.edu.kz>
2024-05-25 13:21:46 +0500

    Initial structure for Backend

---
+++ backend/src/main/java/com/oracle/football/FootballOracleApplication.java
@@ -0,0 +1,13 @@
+package com.oracle.football.football_oracle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballOracleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballOracleApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/oracle/football/FootballOracleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oracle.football.football_oracle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballOracleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
