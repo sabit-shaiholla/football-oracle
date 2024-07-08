8465fdf96f4da3dc6a2d41bd2e6e75a632c97207
sabit-shaiholla <sabit.shaiholla@nu.edu.kz>
2024-07-08 13:42:04 +0500

    feat: allowance of swagger-ui and added response statuses in ExceptionHandler class

--- backend/src/main/java/com/oracle/football/exception/DefaultExceptionHandler.java
+++ backend/src/main/java/com/oracle/football/exception/DefaultExceptionHandler.java
@@ -6,15 +6,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.InsufficientAuthenticationException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.LocalDateTime;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class DefaultExceptionHandler {
 
   @ExceptionHandler(ResourceNotFoundException.class)
+  @ResponseStatus(HttpStatus.NOT_FOUND)
   public ResponseEntity<ApiError> handleException(ResourceNotFoundException e,
       HttpServletRequest request) {
     ApiError apiError = new ApiError(
@@ -27,6 +29,7 @@ public class DefaultExceptionHandler {
   }
 
   @ExceptionHandler(InsufficientAuthenticationException.class)
+  @ResponseStatus(HttpStatus.FORBIDDEN)
   public ResponseEntity<ApiError> handleException(InsufficientAuthenticationException e,
       HttpServletRequest request) {
     ApiError apiError = new ApiError(
@@ -39,6 +42,7 @@ public class DefaultExceptionHandler {
   }
 
   @ExceptionHandler(BadCredentialsException.class)
+  @ResponseStatus(HttpStatus.UNAUTHORIZED)
   public ResponseEntity<ApiError> handleException(BadCredentialsException e,
       HttpServletRequest request) {
     ApiError apiError = new ApiError(
@@ -51,6 +55,7 @@ public class DefaultExceptionHandler {
   }
 
   @ExceptionHandler(Exception.class)
+  @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
   public ResponseEntity<ApiError> handleException(Exception e,
       HttpServletRequest request) {
     ApiError apiError = new ApiError(
@@ -63,6 +68,7 @@ public class DefaultExceptionHandler {
   }
 
   @ExceptionHandler(Selector.SelectorParseException.class)
+  @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
   public ResponseEntity<ApiError> handleException(Selector.SelectorParseException e,
       HttpServletRequest request) {
     ApiError apiError = new ApiError(
